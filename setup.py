0d5e648604506780aa88e6e227c71a1f48a2112d
Hao-Ting Wang <htwangtw@gmail.com>
2020-06-30 21:46:09 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="pyretroicor",
     description=("Python implementation of RETROICOR."),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license="BSD-3-clause",
-    version="0.0.1",
+    version="0.0.2",
     url="https://github.com/htwangtw/pyretroicor",
     author="Hao-Ting Wang",
+    author_email="htwangtw@gmail.com",
     packages=["pyretroicor"],
-    install_requires=["numpy>=1.18"],
+    install_requires=["numpy>=1.18", "nibabel>=2.5"],
     keywords=["fmri", "physiology"],
     zip_safe=True,
     entry_points={
